556c5b28c4f754df52078dff57ede09a51a7af78
Love <lovejansson.94@gmail.com>
2025-02-02 15:40:11 +0100

    default base path /mario

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig, loadEnv } from 'vite'
+
+export default defineConfig(({ command, mode }) => {
+    // Load env file based on `mode` in the current working directory.
+    // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+    const env = loadEnv(mode, process.cwd(), '');
+
+    return {
+        base: env.NODE_ENV === "production" ? "/mario" : "/"
+
+    }
+})
